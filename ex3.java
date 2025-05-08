bccb379ff3d076dfdded8d499570b90ec5cdef1a
Rafael_Ryu <168924432+RafaelRyu@users.noreply.github.com>
2025-05-08 08:36:06 -0300

    exercício novo

--- ex3.java
+++ ex3.java
@@ -1,29 +1,29 @@
+package exercise1;
+
 import java.util.Scanner;
 
 public class ex3 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int x,y;
+		// TODO Auto-generated method stub
+Scanner sc = new Scanner(System.in);
+ int size;
+
+String[] foods;
+System.out.println("Quantos alimentos você deseja inserir ?: ");
+size = sc.nextInt();
+foods = new String[size];
+sc.nextLine();
+
+for (int i = 0; i<foods.length; i++) {
+	System.out.println("Digite um alimento: ");
+	foods[i] = sc.nextLine();
+}
+System.out.println("Seus alimentos são:");	
+for (String food : foods) {
+		System.out.println("- "+food);
 		
-		x= sc.nextInt();
-	    y= sc.nextInt();
-		
-		while (x != y ) {
-			if ( x < y) {
-				System.out.println("Crescente");
-			} else {
-				System.out.println("Decrescente");
-			}
-			x= sc.nextInt();
-		    y= sc.nextInt();
-		}
-		
-		
-		
-		
-		
-		
+}
 		
 		
 	}
